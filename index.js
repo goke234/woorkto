adfdb470dc5c4e7b728cbe3c9633312f8c3fc0fa
goke234 <tifeadegoke454@gmail.com>
2026-06-11 01:33:02 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function fahreheit(fahreheit) {
+    let result = (fahreheit - 32) * 5/9;
+    console.log(result)
+}
+ fahreheit(54)
+
+
+function celsius(celsius){
+    let results = (celsius * 9/5) + 32 ;
+    console.log(results)
+}
+celsius(24)
